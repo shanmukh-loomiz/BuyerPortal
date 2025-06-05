e3c584bf4e44280b317b596453ca7e86a53c7610
asaisathwik <andhe.saisathwik007@gmail.com>
2025-06-05 14:40:03 +0530

    Production trcaking UI

--- src/app/components/OrderTrackingSteps/FabricInhoused.jsx
+++ src/app/components/OrderTrackingSteps/FabricInhoused.jsx
@@ -16,7 +16,7 @@ const FabricInhoused = ({ status = "Not Started", orderData }) => {
     <div className="mx-30 my-10 shadow-md border border-[#E0E0E0] bg-white rounded-[10px]">
       <div className="flex justify-between items-center px-6 py-4">
         <h2 className="font-[NSmedium] text-[#3F72AF] pl-5 text-[24px] font-medium py-2">
-          FABRIC INHOUSED
+          FABRICINHOUSED
         </h2>
         <span className={`text-xs font-medium font-[NSregular] px-2 py-0.5 rounded-full ${getStatusStyle(status)}`}>
           {status.toUpperCase()}
